49236011584caaa6f7e54a7e5c7bbd29bd1d3e11
Hideki Abe <hideki.abe.0630@gmail.com>
2020-11-22 07:49:27 +0900

    generatorのベースを更新

--- test/app.js
+++ test/app.js
@@ -1,17 +1,16 @@
 'use strict';
-var path = require('path');
-var assert = require('yeoman-assert');
-var helpers = require('yeoman-test');
+const path = require('path');
+const assert = require('yeoman-assert');
+const helpers = require('yeoman-test');
 
-describe('generator-skyward-dev:app', () => {
-  before(() => {
-    return helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({someAnswer: true});
-  });
+// Describe('generator-skyward-dev:app', () => {
+//   before(() => {
+//     return helpers
+//       .run(path.join(__dirname, '../generators/app'))
+//       .withPrompts({ someAnswer: true });
+//   });
 
-  it('creates files', () => {
-    assert.file([
-      'dummyfile.txt'
-    ]);
-  });
-});
+//   it('creates files', () => {
+//     assert.file(['dummyfile.txt']);
+//   });
+// });
